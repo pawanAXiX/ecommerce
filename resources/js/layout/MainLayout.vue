ce054c0c8cf49d10ccb777f335b895d6f3d478cc
pawanAXiX <pawanregmi86@gmail.com>
2025-04-08 19:09:18 +0545

    fixed fragments

--- resources/js/layout/MainLayout.vue
+++ resources/js/layout/MainLayout.vue
@@ -1,10 +1,12 @@
 <template>
+    <div class="w-full h-full">
     <Navbar />
     <slot></slot>
+
+    </div>
 </template>
 
 <script setup>
-
 
 import Navbar from "./partials/Navbar.vue";
 </script>
